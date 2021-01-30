2952e975418a9b63fccbaa15f448e90f5234ed6b
Eftekar raghib <mdraghib17@gmail.com>
2021-01-30 15:19:59 +0600

    login frontend validation.

--- src/MyAccount/Login/pageComponents/LoginBody.js
+++ src/MyAccount/Login/pageComponents/LoginBody.js
@@ -3,13 +3,36 @@ import { Link } from 'react-router-dom'
 
 export default function LoginBody() {
 
-    const [email, setEmail] = useState('')
+    const [number, setNumber] = useState('88')
     const [password, setPassword] = useState('')
     const [showValue, setShowValue] = useState(false)
-
+    const [error, setError] = useState({})
+    
+    // show hidden password
     const showPass = (e)=>{
         e.preventDefault()
         setShowValue(!showValue)
+    }
+    const signIn = ()=>{
+        const exp = new RegExp(/[0-9]/)
+        const parsedNum =parseInt(number)
+        if(!parsedNum || parsedNum.toString().length < 13){
+            setError({number:'Exp:01846029691'})
+        }else if(!exp.test(parsedNum)){
+            setError({number:'You need to provide number to login.'})
+            console.log('exp');
+        }else if(!password){
+            setError({password:'You need to provide password to login.'})
+        }else if(!password.length >= 8){
+            setError({password:'password length is null.'})
+        }else{
+            setNumber('88')
+            setPassword('')
+        }
+
+
+        console.log(parsedNum,password,typeof(password));
+
     }
     return (
         <div>
@@ -30,23 +53,25 @@ export default function LoginBody() {
                         <div className="row">
                         <div className="col-12 col-sm-12 col-md-12 col-lg-8 col-xl-6 offset-lg-2 offset-xl-3">
                             <div className="login-form">
-                            <form action="#">
+                            <form action="#" onSubmit= {signIn}>
                                 <div className="form-group row align-items-center mb-4">
-                                <label htmlFor="email" className="col-12 col-sm-12 col-md-4 col-form-label">Email address</label>
+                                <label htmlFor="number" className="col-12 col-sm-12 col-md-4 col-form-label">Your number</label>
                                 <div className="col-12 col-sm-12 col-md-8">
-                                    <input type="text" className="form-control" id="email" placeholder="Email" value={email} onChange={(e)=>setEmail(e.target.value)}  required />
+                                    <input type="text" className="form-control" id="number" placeholder="Put your registered number" value={number} onChange={(e)=>setNumber(e.target.value)}/>
+                                    <span className='text-danger'>{error.number}</span>
                                 </div>
                                 </div>
                                 <div className="form-group row align-items-center mb-4">
                                 <label htmlFor="c-password" className="col-12 col-sm-12 col-md-4 col-form-label">Password</label>
                                 <div className="col-12 col-sm-12 col-md-8">
-                                    <input type={showValue ? "text" : "password"} className="form-control" id="c-password" placeholder="Password" required value={password} onChange={(e)=>setPassword(e.target.value)} />
+                                    <input type={showValue ? "text" : "password"} className="form-control" id="c-password" placeholder="Password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
                                     <button className="pass-show-btn" type="button" onClick={showPass}>Show</button>
+                                    <span className='text-danger'>{error.password}</span>
                                 </div>
                                 </div>
                                 <div className="login-box mt-5 text-center">
                                 <p><Link to="#">Forgot your password?</Link></p>
-                                <button type="submit" className="btn btn-secondary mb-4 mt-4">Sign In</button>
+                                <button type='submit' className="btn btn-secondary mb-4 mt-4">Sign In</button>
                                 </div>
                                 <div className="text-center pt-20 top-bordered">
                                 <p>No account? <Link to="/register">Create one here</Link>.</p>
